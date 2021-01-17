4e72900bdd0b0775490a2bce25ed8d7efd28e256
zhanchao.wu <zhanchao.wu@icloud.com>
2021-01-17 17:14:16 +0800

    vuetify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'vuetify',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-console':
+      process.env.NODE_ENV === 'production'
+        ? 'warn'
+        : 'off',
+    'no-debugger':
+      process.env.NODE_ENV === 'production'
+        ? 'warn'
+        : 'off',
+    'vue/max-attributes-per-line': ['off', 2],
+    'vue/script-indent': ['off', 2],
+    semi: ['off', 2],
+  },
+};
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import zhHans from 'vuetify/src/locale/zh-Hans';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
+import zhHans from 'vuetify/src/locale/zh-Hans'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
     lang: {
@@ -12,4 +12,4 @@ export default new Vuetify({
   icons: {
     iconfont: 'fa',
   },
-});
+})
