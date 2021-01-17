7ed4017d25aea7f101fb2e0c3c10935f97c93a4a
zhanchao.wu <zhanchao.wu@icloud.com>
2021-01-17 17:33:35 +0800

    vuetify eslint @ parserOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
     'vuetify',
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
     'no-console':
       process.env.NODE_ENV === 'production'
@@ -24,5 +28,23 @@ module.exports = {
     'vue/max-attributes-per-line': ['off', 2],
     'vue/script-indent': ['off', 2],
     semi: ['off', 2],
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
+  parserOptions: {
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
   },
 };
