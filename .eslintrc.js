2d7e69f7195ba538a492b601003f12c85351bad2
zhanchao.wu <zhanchao.wu@icloud.com>
2021-01-17 10:19:01 +0800

    plugin

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['plugin:vue/base'],
+  plugins: ['vuetify'],
+  rules: {
+    'vuetify/no-deprecated-classes': 'error',
+    'vuetify/grid-unknown-attributes': 'error',
+    'vuetify/no-legacy-grid': 'error',
+  },
+};
